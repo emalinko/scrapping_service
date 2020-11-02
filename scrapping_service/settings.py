9a8e58d25f7666fb7f869d72dfe21cbbd0b5e1b7
Malinko Evgenii <malin88able@gmail.com>
2020-11-03 01:08:21 +0200

    second commit

--- scrapping_service/settings.py
+++ scrapping_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scraping',
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
